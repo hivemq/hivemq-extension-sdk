73079f4e0d265e7139b3f7117e99758e535e2f94
Silvio Giebl <silvio.giebl@hivemq.com>
2020-06-08 21:13:52 +0200

    build > added utf8 plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement {
     plugins {
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
         id("com.jfrog.bintray") version "${extra["plugin.bintray.version"]}"
+        id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
         id("com.github.sgtsilvio.gradle.metadata") version "${extra["plugin.metadata.version"]}"
     }
 }
