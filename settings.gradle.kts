49e3d438e591c83681088eb1773017dde7cb23e6
yweber <yannick.weber@hivemq.com>
2021-03-24 11:21:01 +0100

    removed bintray plugin configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ rootProject.name = "hivemq-extension-sdk"
 pluginManagement {
     plugins {
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-        id("com.jfrog.bintray") version "${extra["plugin.bintray.version"]}"
         id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
         id("com.github.sgtsilvio.gradle.metadata") version "${extra["plugin.metadata.version"]}"
         id("com.github.sgtsilvio.gradle.javadoc-links") version "${extra["plugin.javadoc-links.version"]}"
