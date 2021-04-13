87de74dc56c05855c91fd05eb38a8318dc709eb7
Silvio Giebl <silvio.giebl@hivemq.com>
2021-04-13 17:26:33 +0200

    build > unification

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,10 @@ rootProject.name = "hivemq-extension-sdk"
 
 pluginManagement {
     plugins {
+        id("io.github.gradle-nexus.publish-plugin") version "${extra["plugin.nexus.publish.version"]}"
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
         id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
         id("com.github.sgtsilvio.gradle.metadata") version "${extra["plugin.metadata.version"]}"
         id("com.github.sgtsilvio.gradle.javadoc-links") version "${extra["plugin.javadoc-links.version"]}"
-        id("io.github.gradle-nexus.publish-plugin") version "${extra["plugin.nexus.publish.version"]}"
     }
 }
