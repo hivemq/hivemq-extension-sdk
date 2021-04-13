d1747a6a2fd40c0de345a4b3567e0739b0203da8
Silvio Giebl <silvio.giebl@hivemq.com>
2021-04-13 17:34:18 +0200

    build > use java toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,8 +74,9 @@ dependencies {
 /* ******************** java ******************** */
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 
     withJavadocJar()
     withSourcesJar()
