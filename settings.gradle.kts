7c9cfbe04354ff56b381c5df2227e57846983bd3
Silvio Giebl <silvio.giebl@hivemq.com>
2021-06-09 17:13:02 +0200

    build > update gradle plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "hivemq-extension-sdk"
 
 pluginManagement {
     plugins {
-        id("io.github.gradle-nexus.publish-plugin") version "${extra["plugin.nexus.publish.version"]}"
+        id("io.github.gradle-nexus.publish-plugin") version "${extra["plugin.nexus-publish.version"]}"
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
         id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
         id("com.github.sgtsilvio.gradle.metadata") version "${extra["plugin.metadata.version"]}"
