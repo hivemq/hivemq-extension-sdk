287069fe46f2b7e131acdd278d7614a68cc7c996
Silvio Giebl <silvio.giebl@hivemq.com>
2022-02-21 22:24:58 +0100

    build > add defaults plugin (and replace utf8 plugin)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
     plugins {
         id("io.github.gradle-nexus.publish-plugin") version "${extra["plugin.nexus-publish.version"]}"
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-        id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
+        id("io.github.sgtsilvio.gradle.defaults") version "${extra["plugin.defaults.version"]}"
         id("com.github.sgtsilvio.gradle.metadata") version "${extra["plugin.metadata.version"]}"
         id("com.github.sgtsilvio.gradle.javadoc-links") version "${extra["plugin.javadoc-links.version"]}"
     }
