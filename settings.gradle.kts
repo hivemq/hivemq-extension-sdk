62a301f86a354cdd12d61c8f61a6ee3bb3d8643f
Silvio Giebl <silvio.giebl@hivemq.com>
2022-02-23 14:23:06 +0100

    build > cleanup plugin ordering ("prod" first, then check plugins)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,10 @@ rootProject.name = "hivemq-extension-sdk"
 pluginManagement {
     plugins {
         id("io.github.gradle-nexus.publish-plugin") version "${extra["plugin.nexus-publish.version"]}"
-        id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
         id("io.github.sgtsilvio.gradle.defaults") version "${extra["plugin.defaults.version"]}"
         id("com.github.sgtsilvio.gradle.metadata") version "${extra["plugin.metadata.version"]}"
         id("com.github.sgtsilvio.gradle.javadoc-links") version "${extra["plugin.javadoc-links.version"]}"
+        id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
     }
 
     if (file("../hivemq/plugins").exists()) {
