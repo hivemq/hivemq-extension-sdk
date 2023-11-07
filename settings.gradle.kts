a3b376d7ddb105abe15b00d235be971aa3ace966
Daniel Krueger <daniel.krueger@hivemq.com>
2023-11-07 14:25:07 +0100

    version updater > start addition

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,12 @@
 pluginManagement {
     if (file("../hivemq/plugins").exists()) {
-        includeBuild("../hivemq/plugins")
+        // includeBuild("../hivemq/plugins")
     }
+    if (file("../hivemq-edge-composite/plugins").exists()) {
+        includeBuild("../hivemq-edge-composite/plugins")
+    }
+
+
 }
 
 rootProject.name = "hivemq-extension-sdk"
